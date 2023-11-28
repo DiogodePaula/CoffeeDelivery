04baaebf713b4a0462093d5658670a75fb747679
Diogo-de-Paula <depaulatrabalhos@gmail.com>
2023-11-27 23:37:58 -0300

    form complete seu pedido criado

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,67 @@
-import { CheckoutContainer } from "./styles";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+
+import { Address1Input, Address2Input, AddressText, CardPayment, CepInput, CheckoutContainer, City1Input, City2Input, City3Input, PaymentContainer, StreetInput } from "./styles";
 
 export function Checkout() {
 	return (
 		<CheckoutContainer>
-			<h1>teste</h1>
+			<div>
+				<h3>Complete seu pedido</h3>
+				<form>
+					<AddressText>
+						<MapPinLine size={24} className="addressIcon" />
+						<div className="addressText">
+							<p>Endereço de Entrega</p>
+							<p>Informe o endereço onde deseja receber seu pedido</p>
+						</div>
+					</AddressText>
+					<CepInput placeholder="CEP" type="number" />
+					<StreetInput placeholder="Rua" type="text" />
+					<div className="address-container">
+						<Address1Input placeholder="Numero" type="text" />
+						<Address2Input placeholder="Complemento" type="text" />
+					</div>
+					<div className="address-container">
+						<City1Input placeholder="Bairro" type="text" />
+						<City2Input placeholder="Cidade" type="text" />
+						<City3Input placeholder="UF" type="text" />
+					</div>
+				</form>
+				<PaymentContainer>
+					<div className="payment-describe">
+						<div>
+							<CurrencyDollar size={24} className="dollarIcon" />
+							<span>Pagamento</span>
+						</div>
+						<p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+					</div>
+					<div className="card-container">
+						<CardPayment>
+							<CreditCard size={24} className="iconColor" />
+							Cartão de crédito
+						</CardPayment>
+						<CardPayment>
+							<Bank size={24} className="iconColor" />
+							cartão de débito
+						</CardPayment>
+						<CardPayment>
+							<Money size={24} className="iconColor" />
+							dinheiro
+						</CardPayment>
+					</div>
+				</PaymentContainer>
+			</div>
+
+			<div>
+				<h3>Cafés selecionados</h3>
+				<div>pedido</div>
+				<div>
+					<p>Total de itens R$ 29,70</p>
+					<p>Total de itens R$ 29,70</p>
+					<p>Total de itens R$ 29,70</p>
+					<button>confirmar pedido</button>
+				</div>
+			</div>
 		</CheckoutContainer>
 	);
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,128 @@
 import styled from "styled-components";
 
-export const CheckoutContainer = styled.main``;
+export const CheckoutContainer = styled.main`
+	display: grid;
+	grid-template-columns: 1fr 28rem;
+	grid-column-gap: 2rem;
+
+	margin-top: 4.5rem;
+
+	h3 {
+		font-family: "Baloo 2";
+		font-weight: 700;
+		font-size: 1.125rem;
+		line-height: 1.4625rem;
+		color: ${(props) => props.theme["brown-700"]};
+	}
+
+	form {
+		padding: 2.5rem;
+		margin-top: 1rem;
+		margin-bottom: 0.75rem;
+		background: ${(props) => props.theme["base-card"]};
+		border-radius: 6px;
+	}
+
+	.address-container {
+		display: flex;
+		gap: 0.75rem;
+	}
+`;
+
+export const AddressText = styled.div`
+	display: flex;
+	gap: 0.5rem;
+	margin-bottom: 2rem;
+
+	.addressIcon {
+		color: ${(props) => props.theme["yellow-700"]};
+	}
+
+	.addressText {
+		p:first-child {
+			color: ${(props) => props.theme["brown-700"]} !important;
+			font-weight: 400;
+			line-height: 1.3rem;
+		}
+	}
+`;
+
+export const BaseInput = styled.input`
+	padding: 12px;
+	border-radius: 4px;
+	border: 1px solid ${(props) => props.theme["base-button"]};
+	background: ${(props) => props.theme["base-input"]};
+	margin-bottom: 1rem;
+`;
+
+export const CepInput = styled(BaseInput)`
+	display: block;
+	width: 200px;
+`;
+
+export const StreetInput = styled(BaseInput)`
+	width: 100%;
+`;
+
+export const Address1Input = styled(BaseInput)`
+	width: 30%;
+`;
+
+export const Address2Input = styled(BaseInput)`
+	width: 70%;
+`;
+export const City1Input = styled(BaseInput)`
+	width: 30%;
+`;
+export const City2Input = styled(BaseInput)`
+	width: 60%;
+`;
+export const City3Input = styled(BaseInput)`
+	width: 10%;
+`;
+
+export const PaymentContainer = styled.div`
+	padding: 2.5rem;
+	border-radius: 6px;
+	background: ${(props) => props.theme["base-card"]};
+
+	.payment-describe {
+		margin-bottom: 2rem;
+
+		div {
+			display: flex;
+			align-items: center;
+			gap: 0.5rem;
+			line-height: 1.3rem;
+
+			.dollarIcon {
+				color: ${(props) => props.theme["purple-700"]};
+			}
+		}
+
+		p {
+			margin-left: 2rem;
+			line-height: 1.3rem;
+		}
+	}
+
+	.card-container {
+		display: flex;
+		gap: 0.75rem;
+	}
+`;
+
+export const CardPayment = styled.div`
+	display: flex;
+	padding: 16px;
+	align-items: center;
+	gap: 12px;
+	flex: 1 0 0;
+	border-radius: 6px;
+	background: ${(props) => props.theme["base-button"]};
+	text-transform: uppercase;
+
+	.iconColor {
+		color: ${(props) => props.theme["purple-700"]};
+	}
+`;
